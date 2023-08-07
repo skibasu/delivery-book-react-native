3bce00ed4e78d6fa5b341165d7f3a4392898a796
Su <skibasu@gmail.com>
2023-08-07 14:08:05 +0200

    DEL-6:splash screen basic setup added

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { Provider } from "react-redux"
 import { store } from "./src/app/store"
 import Main from "./src"
+import * as SplashScreen from "expo-splash-screen"
+
+SplashScreen.preventAutoHideAsync()
+setTimeout(SplashScreen.hideAsync, 2000)
 
 export default function App() {
     return (
